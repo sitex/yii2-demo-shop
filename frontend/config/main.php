0c5a022a9cde3fa471cf4909de7f56ce92349d90
Ivan Sam <gsitex@gmail.com>
2017-05-17 20:25:19 +0700

    Unified authentication

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,15 +14,19 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'cookieValidationKey' => $params['cookieValidationKey'],
         ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain' => $params['cookieDomain']],
         ],
         'session' => [
-            // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
+            'name' => '_session',
+            'cookieParams' => [
+                'domain' => $params['cookieDomain'],
+                'httpOnly' => true,
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -41,7 +45,7 @@ return [
             'showScriptName' => false,
             'rules' => [
                 '' => 'site/index',
-                '<_a:login|logout>' => 'site/<_a>',
+                '<_a:about|contact|signup|login>' => 'site/<_a>',
 
                 '<_c:[\w\-]+>' => '<_c>/index',
                 '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
